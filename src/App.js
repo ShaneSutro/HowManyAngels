208ad8e30715df985212eb0e8d2d94ac194bd9e4
Shane Sutro <shanesutro@gmail.com>
2021-08-31 16:21:53 -0600

    Add angel

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
+import angel from './angel.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>How many angels are dancing on the head of a pin?</h1>
+      <img src={angel} alt="angel"/>
     </div>
   );
 }
